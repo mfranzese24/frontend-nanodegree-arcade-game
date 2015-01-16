339d8c5e508d0335354630bc44488c11a5d77052
Michele Franzese <mfranzese24@me.com>
2015-01-16 11:07:47 -0500

    BACKGROUND IMAGE APPEARS

--- js/app.js
+++ js/app.js
@@ -24,11 +24,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
 
+    this.boy = 'images/char-boy.png';
 
+};
+Player.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+Player.render = function() {
+    ctx.drawImage(Resources.get(this.boy), this.x, this.y);
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+
+var player = Player;
 
 
 
