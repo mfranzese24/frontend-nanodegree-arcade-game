453913960e563466c98bc2fabff5811a9da364c8
Michele Franzese <mfranzese24@me.com>
2015-01-27 19:36:09 -0500

    Make Player Appear on Screen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -26,23 +31,30 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function(){
 
-    this.boy = 'images/char-boy.png';
+    this.sprite = 'images/char-boy.png';
+    this.x = 250;
+    this.y = 400;
 
 };
-Player.update = function(dt) {
+Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+     if (this.y <= 0) { this.y = 100;}
 }
-Player.render = function() {
-    ctx.drawImage(Resources.get(this.boy), this.x, this.y);
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
+
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
 
-var player = Player;
+var player = new Player();
 
 
 
